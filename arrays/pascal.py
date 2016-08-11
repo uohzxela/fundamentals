c604f043c3c51a82ae3a47b89211ba63b2ab8061
uohzxela <uohzxela@gmail.com>
2016-08-11 12:07:37 +0800

    finish up arrays

--- arrays/pascal.py
+++ arrays/pascal.py
@@ -6,5 +6,16 @@ def pascal(n):
 			print c,
 			c = c * (line - i)/i
 		print 
+		
+def pascal2(n):
+	res = [[1]]
+	for i in xrange(1, n):
+		res.append([1])
+		for j in xrange(1, len(res[i-1])):
+			res[i].append(res[i-1][j] + res[i-1][j-1])
+		res[i].append(1)
+	for r in res:
+		print r
 
+pascal(6)
 pascal(6)
--- arrays/spiral_matrix.py
+++ arrays/spiral_matrix.py
@@ -1,28 +1,41 @@
-m = [
+m1 = [
  [ 1, 2, 3, 4],
  [ 5, 6, 7, 8],
  [ 9, 10, 11, 12 ],
  [13, 14, 15, 16]
 ]
-# not finished yet
-def spiral(m):
-	n = len(m)
-	i = n-1
-	res = []
-	while i >= 0:
-		if i == n - i - 1:
-			res.append(m[i][i])
-			break
-		for j in xrange(i):
-			res.append(m[n-i-1][j])
-		for j in xrange(i):
-			res.append(m[j][i])
-		for j in xrange(i):
-			res.append(m[i][i-j])
-		for j in xrange(i):
-			res.append(m[i-j][n-i-1])
-		i -= 1
-	print res
 
-spiral(m)
+m2 = [
+ [ 1, 2, 3],
+ [ 4, 5, 6],
+ [ 7, 8, 9]
+]
+
+m3 = [
+ [ 1, 2],
+ [ 3, 4]
+]
+
+def spiral(m):
+	n = len(m)-1
+	res = []
+	i = 0
+	while n >= 0:
+		if n == 0:
+			res.append(m[i][i])
+		for j in xrange(n):
+			res.append(m[i][i+j])
+		for j in xrange(n):
+			res.append(m[i+j][i+n])
+		for j in xrange(n):
+			res.append(m[i+n][i+n-j])
+		for j in xrange(n):
+			res.append(m[i+n-j][i])
+		i += 1
+		n -= 2
+	return res
+
+assert spiral(m1) == [1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]
+assert spiral(m2) == [1,2,3,6,9,8,7,4,5]
+assert spiral(m3) == [1,2,4,3]
 
