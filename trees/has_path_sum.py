c4ead7e6fbb21aab9fed03ffe2a4d5e00619a4cf
uohzxela <uohzxela@gmail.com>
2016-09-22 11:45:32 +0800

    implement has_path_sum.py

---
+++ trees/has_path_sum.py
@@ -0,0 +1,38 @@
+def has_path_sum(root, target):
+	return has_path_sum_(root, target, 0)
+
+def has_path_sum_(root, target, res):
+	if not root: return False
+	res += root.val
+	if root.left is None and root.right is None:
+		return res == target
+	return (has_path_sum_(root.left, target, res) or 
+			has_path_sum_(root.right,target, res))
+
+class Node(object):
+	def __init__(self, val):
+		self.val = val
+		self.left = None
+		self.right = None
+
+r = Node(314)
+r.left = Node(6)
+r.left.left = Node(271)
+r.left.left.left = Node(28)
+r.left.left.right = Node(0)
+
+r.left.right = Node(561)
+r.left.right.right = Node(3)
+r.left.right.right.left = Node(17)
+
+r.right = Node(6)
+r.right.left = Node(2)
+r.right.left.right = Node(1)
+r.right.left.right.left = Node(401)
+r.right.left.right.right = Node(257)
+r.right.left.right.left.right = Node(641)
+
+r.right.right = Node(271)
+r.right.right.right = Node(28)
+
+assert has_path_sum(r, 591)
--- trees/sum_root_to_leaf_paths.py
+++ trees/sum_root_to_leaf_paths.py
@@ -4,38 +4,38 @@ def sum_root_to_leaf_paths(root):
 def dfs(root, res):
 	if not root: return 0
 	res = res*2+root.val
-	if not root.l and not root.r:
+	if not root.left and not root.right:
 		return res
-	left = dfs(root.l, res)
-	right = dfs(root.r, res)
+	left = dfs(root.left, res)
+	right = dfs(root.right, res)
 	return left + right
 
 class Node(object):
 	def __init__(self, val):
 		self.val = val
-		self.l = None
-		self.r = None
+		self.left = None
+		self.right = None
 
 n = Node(1)
-n.l = Node(0)
-n.r = Node(1)
+n.left = Node(0)
+n.right = Node(1)
 
-n.l.l = Node(0)
-n.l.r = Node(1)
+n.left.left = Node(0)
+n.left.right = Node(1)
 
-n.l.l.l = Node(0)
-n.l.l.r = Node(1)
+n.left.left.left = Node(0)
+n.left.left.right = Node(1)
 
-n.l.r.r = Node(1)
-n.l.r.r.l = Node(0)
+n.left.right.right = Node(1)
+n.left.right.right.left = Node(0)
 
-n.r.l = Node(0)
-n.r.r = Node(0)
-n.r.l.r = Node(0)
-n.r.l.r.l = Node(1)
-n.r.l.r.r = Node(0)
-n.r.l.r.l.r = Node(1)
+n.right.left = Node(0)
+n.right.right = Node(0)
+n.right.left.right = Node(0)
+n.right.left.right.left = Node(1)
+n.right.left.right.right = Node(0)
+n.right.left.right.left.right = Node(1)
 
-n.r.r.r = Node(0)
+n.right.right.right = Node(0)
 
 assert sum_root_to_leaf_paths(n) == 126
