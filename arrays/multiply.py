0e71b8ddebbb02349f2b46e75b0a7afaa83d2818
uohzxela <uohzxela@gmail.com>
2016-09-24 11:55:44 +0800

    do multiplication from backwards to avoid reversing

--- arrays/multiply.py
+++ arrays/multiply.py
@@ -1,22 +1,21 @@
 """
-similar to strings/multiply_bigint.py
+similar to strings/multiply_bigint.py, but the latter uses the reverse method
 """
 def multiply(A, B):
 	sign = -1 if A[0] < 0 or B[0] < 0 else 1
 	A[0] = abs(A[0])
 	B[0] = abs(B[0])
-	A.reverse()
-	B.reverse()
+
 	res = [0] * (len(A) + len(B))
-	for i in xrange(len(A)):
-		carry = 0
-		for j in xrange(len(B)):
-			res[i+j] += carry + (A[i] * B[j])
-			carry = res[i+j]/10
-			res[i+j] %= 10
-		res[i+len(B)] += carry
-	if res[-1] == 0: res = res[:-1]
-	res.reverse()
+	for i in xrange(len(A)-1, -1, -1):
+		for j in xrange(len(B)-1, -1, -1):
+			res[i+j+1] += A[i] * B[j]
+			# shift carry forward
+			res[i+j] += res[i+j+1]/10
+			res[i+j+1] %= 10
+
+	if res[0] == 0:
+		res = res[1:]
 	res[0] *= sign
 	return res
 
