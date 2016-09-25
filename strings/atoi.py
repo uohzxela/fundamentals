41f425a11ac48ff388837a15633793fe361e9116
uohzxela <uohzxela@gmail.com>
2016-09-25 12:25:34 +0800

    clean up atoi.py

--- strings/atoi.py
+++ strings/atoi.py
@@ -1,14 +1,19 @@
 def atoi(s):
-	if not s: return 0
-	res = 0
 	s = s.strip()
-	n = len(s)
-	isNeg = s[0] == '-'
-	start = 1 if isNeg or s[0] == '+' else 0
-	for i in xrange(start, n):
-		if ord(s[i]) < ord('0') or ord(s[i]) > ord('9') :  break
-		res = res * 10 + (ord(s[i])- ord('0'))
-	return -res if isNeg else res
+	if not s:
+		return 0
+	res = 0
+	sign = -1 if s[0] == '-' else 1
+	start = 1 if not is_digit(s[0]) else 0
+	for i in xrange(start, len(s)):
+		if not is_digit(s[i]): 
+			break
+		res = res * 10 + (ord(s[i]) - ord('0'))
+	return res * sign
+
+def is_digit(c):
+	return ord(c) >= ord('0') and ord(c) <= ord('9')
 
 assert atoi("2147483648") == 2147483648
 assert atoi("-123") == -123
+assert atoi("+123") == 123
