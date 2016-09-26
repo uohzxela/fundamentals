7795b4c5c640b1718ccff717420a0c8b53d1276b
uohzxela <uohzxela@gmail.com>
2016-09-26 11:04:10 +0800

    implement roman to decimal conversion

---
+++ strings/roman_to_decimal.py
@@ -0,0 +1,20 @@
+def roman_to_decimal(s):
+	T = {
+		"I": 1,
+		"V": 5,
+		"X": 10,
+		"L": 50,
+		"C": 100,
+		"D": 500,
+		"M": 1000
+	}
+	decimal = T[s[-1]]
+	for i in xrange(len(s)-2, -1, -1):
+		if T[s[i]] < T[s[i+1]]:
+			decimal -= T[s[i]]
+		else:
+			decimal += T[s[i]]
+	return decimal
+
+assert roman_to_decimal("IC") == 99
+assert roman_to_decimal("LIX") == 59
