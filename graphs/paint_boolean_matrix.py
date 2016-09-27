3cb411483aba3463370e8faa6e8eb21a4078248a
uohzxela <uohzxela@gmail.com>
2016-09-27 12:30:24 +0800

    add DFS version for paint_boolean_matrix.py

--- graphs/paint_boolean_matrix.py
+++ graphs/paint_boolean_matrix.py
@@ -12,7 +12,8 @@ M2 = [
 [0, 1],
 [0, 0]
 ]
-def paint(M, x, y):
+
+def paintBFS(M, x, y):
 	q = deque([(x, y)])
 	prev_color = M[x][y]
 	M[x][y] ^= 1
@@ -28,6 +29,20 @@ def paint(M, x, y):
 			q.append((i, j))
 			M[i][j] ^= 1
 
+
+def paintDFS(M, x, y):
+	prev_color = M[x][y]
+	M[x][y] ^= 1
+	for i, j in [(x, y-1), (x, y+1), (x+1, y), (x-1, y)]:
+		if i < 0 or i > len(M)-1 or j < 0 or j > len(M[0])-1:
+			continue
+		if M[i][j] != prev_color:
+			continue
+		# must flip current cell before pushing to queue
+		# so as to mark it as visited
+		paintDFS(M, i, j)
+
+
 def print_matrix(M):
 	for i in xrange(len(M)):
 		for j in xrange(len(M[0])):
@@ -35,6 +50,7 @@ def print_matrix(M):
 		print
 	print
 
+
 print_matrix(M)
-paint(M, 2, 2)
+paintBFS(M, 2, 2)
 print_matrix(M)
