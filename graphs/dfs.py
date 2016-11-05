33ab6e948818c00ef33a64650d15c357c4008559
uohzxela <uohzxela@gmail.com>
2016-11-05 11:12:01 +0800

    refactor dfs

--- graphs/dfs.py
+++ graphs/dfs.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 g2 = {
     0 : [1,2,3],
     1 : [4,5],
@@ -10,16 +8,18 @@ g2 = {
 }
 
 def dfs(g):
-    visited = defaultdict(lambda: None)
+    visited = set()
     for v in g:
-        if not visited[v]: 
-            dfs_(g, v, visited)
+        if v in visited:
+            continue
+        dfs_(g, v, visited)
 
 def dfs_(g, v, visited):
-    visited[v] = True
+    visited.add(v)
     print v,
     for n in g[v]:
-        if not visited[n]:
-            dfs_(g, n, visited)
+        if n in visited:
+            continue
+        dfs_(g, n, visited)
     
 dfs(g2)
