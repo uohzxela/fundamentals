ce80ef0eb0437e9b9e51b7bac9654d6c604a0f63
uohzxela <uohzxela@gmail.com>
2016-11-05 10:53:20 +0800

    refactor toposort

--- graphs/topological_sort.py
+++ graphs/topological_sort.py
@@ -1,30 +1,33 @@
 from collections import defaultdict
 
-# 1->2->3
-g = {
-	5: [2, 0],
-	4: [0, 1],
-	3: [1],
-	2: [3]
-}
+G = defaultdict(list)
 
-def toposort(g):
-	visited = defaultdict(lambda: None)
+G[5] = [11]
+G[7] = [11, 8]
+G[3] = [8, 10]
+G[11] = [2, 9, 10]
+G[8] = [9]
+G[2] = []
+G[9] = []
+G[10] = []
+
+def toposort(G):
+	visited = set()
 	stack = []
-	for k,v in g.iteritems():
-		if not visited[k]:
-			toposort_(g, stack, visited, k)
+	for v in G:
+		if v in visited:
+			continue
+		toposort_(G, v, visited, stack)
 	while stack:
 		print stack.pop(),
 	print
 
-def toposort_(g, stack, visited, v):
-	visited[v] = True
-	if v in g:
-		for n in g[v]:
-			if not visited[n]: 
-				toposort_(g, stack, visited, n)
+def toposort_(G, v, visited, stack):
+	visited.add(v)
+	for n in G[v]:
+		if n in visited:
+			continue
+		toposort_(G, n, visited, stack)
 	stack.append(v)
 
-toposort(g)
-
+toposort(G)
