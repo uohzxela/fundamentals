164349b1d9c7e20be5a020a3b293853eee42d2c5
uohzxela <uohzxela@gmail.com>
2016-11-05 11:09:39 +0800

    refactor bfs

--- graphs/bfs.py
+++ graphs/bfs.py
@@ -1,26 +1,28 @@
-from collections import defaultdict, deque
+from collections import deque
+
 def bfs(g):
-	visited = defaultdict(lambda: None)
+	visited = set()
 	q = deque()
 
 	for v in g:
-		if not visited[v]:
-			visited[v] = True
-			q.append(v)
+		if v in visited:
+			continue
+		visited.add(v)
+		q.append(v)
 		while q:
-			n = q.popleft()
+			v = q.popleft()
 			# don't mark it as visited after removing from queue
 			# or else there will bugs related to duplicate nodes!
-			print n,
-			for neighbor in g[n]:
-				if visited[neighbor]:
+			print v,
+			for n in g[v]:
+				if n in visited:
 					continue
 				# important! 
 				# mark neighbor as visited before pushing it into queue
 				# this is to avoid duplicate neighbors being pushed into the queue
 				# in the following iterations of while loop
-				visited[n] = True
-				q.append(neighbor)
+				visited.add(n)
+				q.append(n)
 
 g2 = {
     0 : [1,2,3],
