9ef385c79d55ca05c89f032535bcc8587ffb9e04
uohzxela <uohzxela@gmail.com>
2017-02-10 14:50:49 +0800

    refactor add.py

--- bit_manipulation/add.py
+++ bit_manipulation/add.py
@@ -1,20 +1,21 @@
-def add(a, b):
-	tempA = a
-	tempB = b
-	sum = carryin = 0
+def add(x, y):
 	k = 1
-	while tempA or tempB:
-		ak = a & k
-		bk = b & k 
-		carryout = (ak & bk) | (ak & carryin) | (bk & carryin)
-		sum |= (ak ^ bk ^ carryin)
-		tempA >>= 1
-		tempB >>= 1
+	x_temp = x
+	y_temp = y
+	res = carry = 0
+	while x_temp or y_temp:
+		xk = k & x
+		yk = k & y
+		res |= xk ^ yk ^ carry
+		carry = (xk & yk) | (xk & carry) | (yk & carry)
+		carry <<= 1
 		k <<= 1
-		carryin = carryout << 1
-	return sum | carryin
+		x_temp >>= 1
+		y_temp >>= 1
+	return res | carry
 
-
-assert add(1,2) == 3
+assert add(3, 6) == 9
+assert add(3, 3) == 6
+assert add(1, 2) == 3
 assert add(10, 52) == 62
 assert add(3, 7) == 10
