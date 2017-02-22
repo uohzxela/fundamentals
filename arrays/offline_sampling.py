6dc5e2cd9c8ea5bea3dd8705afb3772aa46fb61c
uohzxela <uohzxela@gmail.com>
2017-02-22 12:57:08 +0800

    implement offline sampling

---
+++ arrays/offline_sampling.py
@@ -0,0 +1,10 @@
+import random
+
+def offline_sampling(A, k):
+	for i in xrange(k):
+		# generate a random int in [i, len(A)-1]
+		target = random.randint(i, len(A)-1)
+		A[i], A[target] = A[target], A[i]
+	return A
+
+print offline_sampling([1,2,3,4,5,6,7,8], 5)
