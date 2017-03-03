bc81e09319c6ed91408dcd4b644518a31729a701
uohzxela <uohzxela@gmail.com>
2017-03-03 14:41:57 +0800

    refactor spiral_matrix.py

--- arrays/spiral_matrix.py
+++ arrays/spiral_matrix.py
@@ -20,9 +20,7 @@ def spiral(m):
 	n = len(m)-1
 	res = []
 	i = 0
-	while n >= 0:
-		if n == 0:
-			res.append(m[i][i])
+	while n > 0:
 		for j in xrange(n):
 			res.append(m[i][i+j])
 		for j in xrange(n):
@@ -33,9 +31,10 @@ def spiral(m):
 			res.append(m[i+n-j][i])
 		i += 1
 		n -= 2
+	if n == 0:
+		res.append(m[i][i])
 	return res
 
 assert spiral(m1) == [1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]
 assert spiral(m2) == [1,2,3,6,9,8,7,4,5]
 assert spiral(m3) == [1,2,4,3]
-
