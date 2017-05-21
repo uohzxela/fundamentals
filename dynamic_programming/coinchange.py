5ed0764c9319a183140e79edcd3e49e99a91248a
uohzxela <uohzxela@gmail.com>
2017-05-21 10:40:38 +0800

    update coinchange with 2D DP and recursive solutions

--- dynamic_programming/coinchange.py
+++ dynamic_programming/coinchange.py
@@ -1,17 +1,18 @@
 """
-You are given coins of different denominations and a total amount of money.
-Write a function to compute the number of combinations that make up that amount.
-You may assume that you have infinite number of each kind of coin.
+You are given coins of different denominations and a total amount of money amount.
+Write a function to compute the fewest number of coins that you need to make up that amount.
+If that amount of money cannot be made up by any combination of the coins, return -1.
 """
 class Solution(object):
-    def change(self, amount, coins):
+    def coinChange(self, coins, amount):
         """
-        :type amount: int
         :type coins: List[int]
+        :type amount: int
         :rtype: int
         """
-        dp = [0 for i in xrange(amount+1)]
-        dp[0] = 1
+        dp = [sys.maxint for j in xrange(amount+1)]
+        dp[0] = 0
+
         for coin in coins:
         	"""
         	Note that the inner loop is not reversed (contrast this with knapsack.py)
@@ -21,5 +22,28 @@ class Solution(object):
         	Vice-versa, when we don't reverse the inner loop, we can reuse the current coin infinitely.
         	"""
             for i in xrange(coin, amount+1):
-                dp[i] += dp[i-coin]
-        return dp[-1]
+                dp[i] = min(dp[i-coin]+1, dp[i])
+        return -1 if dp[-1] == sys.maxint else dp[-1]
+    
+    def coinChange2D(self, coins, amount):
+        dp = [[sys.maxint for j in xrange(amount+1)] for i in xrange(len(coins)+1)]
+        for i in xrange(len(coins)+1):
+            dp[i][0] = 0
+        
+        for i in xrange(1, len(coins)+1):
+            for j in xrange(1, amount+1):
+                dp[i][j] = dp[i-1][j]
+                if j-coins[i-1] >= 0:
+                    dp[i][j] = min(dp[i][j-coins[i-1]] + 1, dp[i-1][j])
+        return -1 if dp[-1][-1] == sys.maxint else dp[-1][-1]
+    
+    def coinChangeRecursive(self, coins, amount, count, i):
+        if amount < 0:
+            return sys.maxint
+        if i == len(coins):
+            if amount == 0:
+                return count
+            return sys.maxint
+        res1 = self.coinChangeRecursive(coins, amount, count, i+1)
+        res2 = self.coinChangeRecursive(coins, amount-coins[i], count+1, i)
+        return min(res1, res2)
