fe50e1a2989c9705034de142792ce5199aa1b2a9
uohzxela <uohzxela@gmail.com>
2017-05-22 10:45:38 +0800

    update longest palin subsequence solution

--- strings/longest_palin_subseq.py
+++ strings/longest_palin_subseq.py
@@ -1,18 +1,30 @@
-def  lps(s):
-    n = len(s)
-    dp = [[0 for x in xrange(n)] for x in xrange(n)]
-    for i in xrange(n):
-        dp[i][i] = 1
-    
-    for sublen in xrange(2, n+1):
-        for i in xrange((n-sublen) + 1):
-            j = i + (sublen-1)
-            if s[i] == s[j] and sublen == 2:
-                dp[i][j] = 2
-            elif s[i] == s[j]:
-                dp[i][j] = dp[i+1][j-1] + 2
-            else:
-                dp[i][j] = max(dp[i][j-1], dp[i+1][j])
-    return dp[0][-1]
-
-assert lps("BBABCBCAB") == 7
+class Solution(object):
+    def longestPalindromeSubseq(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+        n = len(s)
+        dp = [[0 for j in xrange(n)] for i in xrange(n)]
+        for i in xrange(n):
+            dp[i][i] = 1
+        for sublen in xrange(1, n+1):
+            for l in xrange(n-sublen):
+                r = l + sublen
+                if s[l] == s[r]:
+                    dp[l][r] = 2 + dp[l+1][r-1]
+                else:
+                    dp[l][r] = max(dp[l+1][r], dp[l][r-1])
+        return dp[0][-1]
+        
+    def helper(self, s, l, r):
+        if l == r:
+            return 1
+        if l > r:
+            return 0
+        if s[l] == s[r]:
+            return 2 + self.helper(s, l+1, r-1)
+        else:
+            return max(self.helper(s, l+1, r), self.helper(s, l, r-1))
