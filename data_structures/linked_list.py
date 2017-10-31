c5bb9a7207518594676e1cf14ed8a2242fc492d5
uohzxela <uohzxela@gmail.com>
2017-10-31 09:06:55 +0800

    Refactor linked_list.py with helper function 'find'

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -1,61 +1,62 @@
 class LinkedList(object):
-	def __init__(self):
-		self.head = None
-		self.size = 0
+    def __init__(self):
+        self.head = None
+        self.size = 0
 
-	def insert_beginning(self, val):
-		"""Time complexity: O(1)"""
-		node = Node(val)
-		node.next = self.head
-		self.head = node
-		self.size += 1
+    def push(self, val):
+        """Time complexity: O(1)"""
+        node = Node(val)
+        node.next = self.head
+        self.head = node
+        self.size += 1
 
-	def remove(self, val):
-		"""Time complexity: O(n)"""
-		prev = None
-		curr = self.head
-		while curr:
-			if curr.val == val:
-				if prev is None:
-					self.head = curr.next
-				else:
-					prev.next = curr.next
-				self.size -= 1
-				return True
-			prev = curr
-			curr = curr.next
-		return False
+    def remove(self, val):
+        """Time complexity: O(N)"""
+        prev, curr = self.find(val)
+        if curr is None:
+            return False
+        if prev is None:
+            self.head = curr.next
+        else:
+            prev.next = curr.next
+        self.size -= 1
+        return True
 
-	def contains(self, val):
-		"""Time complexity: O(n)"""
-		curr = self.head
-		while curr:
-			if curr.val == val:
-				return True
-			curr = curr.next
-		return False
+    def contains(self, val):
+        """Time complexity: O(N)"""
+        _, curr = self.find(val)
+        return curr is not None
 
-	def print_list(self):
-		curr = self.head
-		while curr:
-			print curr.val,
-			curr = curr.next
-		print
+    def find(self, val):
+        """Time complexity: O(N)"""
+        prev, curr = None, self.head
+        while curr:
+            if curr.val == val:
+                return prev, curr
+            prev, curr = curr, curr.next
+        return prev, curr
 
-	def get_size(self):
-		return self.size
+    def print_list(self):
+        curr = self.head
+        while curr:
+            print curr.val,
+            curr = curr.next
+        print
+
+    def get_size(self):
+        return self.size
 
 
 class Node(object):
-	def __init__(self, val=None, next=None):
-		self.val = val
-		self.next = next
+    def __init__(self, val=None, next=None):
+        self.val = val
+        self.next = next
 
 llist = LinkedList()
-llist.insert_beginning(1)
-llist.insert_beginning(2)
-llist.insert_beginning(3)
-llist.insert_beginning(4)
+llist.push(1)
+llist.push(2)
+llist.push(3)
+llist.push(4)
 llist.print_list()
 assert llist.contains(2)
 print "After removing 2"
