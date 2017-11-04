00abeecf8874204aa402ecb2d138c69aea58039b
uohzxela <uohzxela@gmail.com>
2017-11-05 00:57:57 +0800

    Refine mergesort

--- sorting/mergesort.py
+++ sorting/mergesort.py
@@ -1,23 +1,29 @@
 def mergesort(A):
-	if len(A) <= 1:
-		return A
-	m = len(A)/2
-	left = mergesort(A[:m])
-	right = mergesort(A[m:])
-	return merge(left, right)
+    if len(A) <= 1:
+        return A
+
+    m = len(A)/2
+    left = mergesort(A[:m])
+    right = mergesort(A[m:])
+
+    return merge(left, right)
+
 
 def merge(left, right):
-	res = []
-	n = m = 0
-	while n < len(left) and m < len(right):
-		if left[n] <= right[m]:
-			res.append(left[n])
-			n += 1
-		else:
-			res.append(right[m])
-			m += 1
-	res += left[n:]
-	res += right[m:]
-	return res 
+    res = []
+    p1 = p2 = 0
 
-print mergesort([23, 21, 5, 9, 10, 4, 1, 8, 3, 0,-1, -10])
+    while p1 < len(left) and p2 < len(right):
+        if left[p1] <= right[p2]:
+            res.append(left[p1])
+            p1 += 1
+        else:
+            res.append(right[p2])
+            p2 += 1
+
+    res += left[p1:]
+    res += right[p2:]
+
+    return res
+
+assert mergesort([23, 21, 5, 9, 10, 4, 1, 8, 3, 0, -1, -10]) == sorted([23, 21, 5, 9, 10, 4, 1, 8, 3, 0, -1, -10])
