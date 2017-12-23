a2f7b1d3110f9a51fcee860f45f52db526d64ab2
uohzxela <uohzxela@gmail.com>
2017-12-23 12:53:49 +0800

    Refactor dutch_flag_partition.py

--- arrays/dutch_flag_partition.py
+++ arrays/dutch_flag_partition.py
@@ -1,23 +1,23 @@
-RED = 0
-WHITE = 1
-BLUE = 2
+def dutch_flag_partition(A, pivot_index):
+	pivot = A[pivot_index]
 
-def dutch_flag_partition(A, p):
 	smaller = 0
 	for i in xrange(len(A)):
-		if A[i] < p:
+		if A[i] < pivot:
 			A[i], A[smaller] = A[smaller], A[i]
 			smaller += 1
 
 	larger = len(A) - 1
 	for i in xrange(len(A)-1, -1, -1):
-		if A[i] < p: break
-		if A[i] > p:
+		if A[i] < pivot:
+			break
+		if A[i] > pivot:
 			A[i], A[larger] = A[larger], A[i]
 			larger -= 1
 
 	return A
 
-assert dutch_flag_partition([2,2,1,0,0,1,2,0], WHITE) == [0,0,0,1,1,2,2,2]
-print dutch_flag_partition([2,2,0,0,1,1,2,0], RED)
-print dutch_flag_partition([2,2,0,0,1,1,2,0], BLUE)
+assert dutch_flag_partition([2,2,1,0,0,1,2,0], 2) == [0,0,0,1,1,2,2,2]
+assert dutch_flag_partition([2,2,1,2,0,1,0,0], 2) == [0,0,0,1,1,2,2,2]
+assert dutch_flag_partition([2,2,0,0,1,1,2,0], 0) == [0, 0, 1, 1, 0, 2, 2, 2]
+assert dutch_flag_partition([2,2,0,0,1,1,2,0], 2) == [0, 0, 0, 2, 2, 1, 1, 2]
