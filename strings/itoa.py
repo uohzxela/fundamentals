05227887e23b86a7e7456ff8b179c484b55198f7
uohzxela <uohzxela@gmail.com>
2017-12-30 18:28:44 +0800

    Refactor itoa.py

--- strings/itoa.py
+++ strings/itoa.py
@@ -1,14 +1,14 @@
 def itoa(n):
 	s = []
-	isNeg = False
+	is_neg = False
 	if n < 0:
-		isNeg = True
+		is_neg = True
 		n = abs(n)
 	while n:
 		c = n % 10
 		s.append(chr(c + ord('0')))
 		n /= 10
-	if isNeg: s.append('-')
+	if is_neg: s.append('-')
 	s.reverse()
 	return ''.join(s)
 
