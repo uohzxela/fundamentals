08c9c31f53cdfeb818488512ff33e3e6ddaea06e
uohzxela <uohzxela@gmail.com>
2017-12-30 19:50:25 +0800

    Refactor reverse_words.py

--- strings/reverse_words.py
+++ strings/reverse_words.py
@@ -1,19 +1,20 @@
 def reverse_words(words):
 	A = list(words)
-	reverse(A, 0, len(A)-1)
+	A.reverse()
 	i = 0
 	while i < len(A):
 		s = i
 		while i < len(A) and A[i] != ' ':
 			i += 1
-		reverse(A, s, i-1)
+		reverse_range(A, s, i-1)
 		while i < len(A) and A[i] == ' ':
 			i += 1
 	return "".join(A)
 
-def reverse(A, s, e):
-	for i in xrange((e-s+1)/2):
-		A[s+i], A[e-i] = A[e-i], A[s+i]
+def reverse_range(A, s, e):
+	while s < e:
+		A[s], A[e] = A[e], A[s]
+		s += 1; e -= 1
 
 assert reverse_words("Alice likes Bob") == "Bob likes Alice"
 assert reverse_words("ram is costly") == "costly is ram"
