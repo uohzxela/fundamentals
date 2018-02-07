1da2320af208ff366164bd309a7e26895c67e364
uohzxela <uohzxela@gmail.com>
2018-02-07 08:15:13 +0800

    Fix bug in towers_of_hanoi.py

--- recursion/towers_of_hanoi.py
+++ recursion/towers_of_hanoi.py
@@ -3,8 +3,8 @@ def move_towers(n):
 
 def move_towers_(n, src, dest, spare):
 	if n > 0:
-		move_towers_(n-1, src, spare, dest)
-		print "move disk", n, 'from', src, "to", dest
-		move_towers_(n-1, src, dest, spare)
+		move_towers_(n - 1, src, spare, dest)
+		print 'move disk', n, 'from', src, 'to', dest
+		move_towers_(n - 1, spare, dest, src)
 
 move_towers(3)
