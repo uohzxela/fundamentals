c9724261dc733a480c46b4f6452ac14642783f79
uohzxela <uohzxela@gmail.com>
2018-02-24 10:51:37 +0800

    Improve solution for edit_distance.py

--- dynamic_programming/edit_distance.py
+++ dynamic_programming/edit_distance.py
@@ -1,37 +1,35 @@
-def edit(s1, s2):
-	return edit_(s1, s2, len(s1)-1, len(s2)-1, 0)
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        dp = [[0 for j in xrange(len(word2) + 1)] for i in xrange(len(word1) + 1)]
+        for i in xrange(len(word1) + 1):
+            dp[i][0] = i
+        for j in xrange(len(word2) + 1):
+            dp[0][j] = j
 
-def edit_(s1, s2, p1, p2, d):
-	if p1 < 0: return d + p2 + 1
-	if p2 < 0: return d + p1 + 1
-	if s1[p1] == s2[p2]:
-		return edit_(s1, s2, p1-1, p2-1, d)
-	insert = edit_(s1, s2, p1-1, p2, d+1)
-	remove = edit_(s1, s2, p1, p2-1, d+1)
-	replace = edit_(s1, s2, p1-1, p2-1, d+1)
-	return min(insert, remove, replace)
+        for i in xrange(1, len(word1) + 1):
+            for j in xrange(1, len(word2) + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    insert, remove, replace = dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]
+                    dp[i][j] = min(insert, remove, replace) + 1
 
-def editDP(s1, s2):
-	dp = [[0 for j in xrange(len(s2)+1)] for i in xrange(len(s1)+1)]
-	for i in xrange(1, len(s1)+1):
-		dp[i][0] = i
-	for i in xrange(1, len(s2)+1):
-		dp[0][i] = i
-	for i in xrange(1, len(s1)+1):
-		for j in xrange(1, len(s2)+1):
-			if s1[i-1] == s2[j-1]:
-				dp[i][j] = dp[i-1][j-1]
-			else:
-				insert = dp[i-1][j] + 1
-				remove = dp[i][j-1] + 1
-				replace = dp[i-1][j-1] + 1
-				dp[i][j] = min(insert, remove, replace)
-	return dp[-1][-1]
+        return dp[-1][-1]
+    
+    def helper(self, word1, word2, i, j):
+        if i < 0:
+            return j + 1
+        if j < 0:
+            return i + 1
+        if word1[i] == word2[j]:
+            return self.helper(word1, word2, i - 1, j - 1)
 
-assert edit("geek", "gesek") == 1
-assert edit("cat", "cut") == 1
-assert edit("sunday", "saturday") == 3
-
-assert editDP("geek", "gesek") == 1
-assert editDP("cat", "cut") == 1
-assert editDP("sunday", "saturday") == 3
+        insert = self.helper(word1, word2, i - 1, j)
+        remove = self.helper(word1, word2, i, j - 1)
+        replace = self.helper(word1, word2, i - 1, j - 1)
+        return min(insert, remove, replace) + 1
