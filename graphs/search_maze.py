64a8c43725cf9d16d07ca2cdb5012c4586d8cfc6
uohzxela <uohzxela@gmail.com>
2018-03-01 08:27:08 +0800

    Refactor search_maze.py

--- graphs/search_maze.py
+++ graphs/search_maze.py
@@ -11,30 +11,31 @@ M = [
 BLOCKED = 1
 
 def search_maze(M):
-	path = [(len(M)-1, 0)]
-	if search_maze_(M, len(M)-1, 0, path):
+	path = [(len(M) - 1, 0)]
+	if search(M, len(M) - 1, 0, path):
 		print path
 	else:
-		print "No path found."
+		print 'No path found.'
 
-def search_maze_(M, x, y, path):
+def search(M, x, y, path):
+	if exceeds_boundary(M, x, y) or M[x][y] == BLOCKED:
+		return False
 	# found exit
-	if x == 0 and y == len(M[0])-1:
+	if x == 0 and y == len(M[0]) - 1:
 		return True
-	for i, j in [(x, y-1), (x, y+1), (x+1, y), (x-1, y)]:
-		# check if (x,y) exceeds boundary
-		if i < 0 or i > len(M)-1 or j < 0 or j > len(M[0])-1:
-			continue
-		if M[i][j] == BLOCKED:
-			continue
+
+	# set current cell to BLOCKED to avoid visiting it again
+	# or you can pass in 'visited' hashmap as param
+	M[x][y] = BLOCKED
+ 	for i, j in [(x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1)]:
 		path.append((i, j))
-		# set current cell to BLOCKED to avoid visiting it again
-		# or you can pass in a "visited" hashmap as an extra param
-		M[i][j] = BLOCKED
-		if search_maze_(M, i, j, path):
+		if search(M, i, j, path):
 			return True
 		# remember to backtrack!
 		path.pop()
 	return False
+
+def exceeds_boundary(M, x, y):
+	return x < 0 or y < 0 or x > len(M) - 1 or y > len(M[0]) - 1
 
 search_maze(M)
