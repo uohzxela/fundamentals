51055327113839e15c3103f6674893a192b6b4e2
uohzxela <uohzxela@gmail.com>
2018-03-04 10:37:33 +0800

    Add alternative solution to rotate_matrix.py

--- arrays/rotate_matrix.py
+++ arrays/rotate_matrix.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 def rotate(M):
 	n = len(M)
 	print 'before:'
@@ -19,10 +20,30 @@ def rotate(M):
 	print
 	return M
 
+def rotate2(A):
+	n = len(A)
+	# swap A[i][j] and A[j][i]
+	for i in xrange(n):
+		for j in xrange(i, n):
+			A[i][j], A[j][i] = A[j][i], A[i][j]
+	# reverse each row
+	for i in xrange(n):
+		s, e = 0, n - 1
+		while s < e:
+			A[i][s], A[i][e] = A[i][e], A[i][s]
+			s, e = s + 1, e - 1
+	return A
+
 M0 = [[1,2],[3,4]]
 M1 = [[1,2,3],[4,5,6],[7,8,9]]
 M2 = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
 
-assert rotate(M0) == [[3,1],[4,2]]
-assert rotate(M1) == [[7,4,1],[8,5,2],[9,6,3]]
-assert rotate(M2) == [[13,9,5,1],[14,10,6,2],[15,11,7,3],[16,12,8,4]]
+# executing these will mutate the matrices so they are commented out
+
+# assert rotate(M0) == [[3,1],[4,2]]
+# assert rotate(M1) == [[7,4,1],[8,5,2],[9,6,3]]
+# assert rotate(M2) == [[13,9,5,1],[14,10,6,2],[15,11,7,3],[16,12,8,4]]
+
+assert rotate2(M0) == [[3,1],[4,2]]
+assert rotate2(M1) == [[7,4,1],[8,5,2],[9,6,3]]
+assert rotate2(M2) == [[13,9,5,1],[14,10,6,2],[15,11,7,3],[16,12,8,4]]
