a0be36b11480d941a6295cd6d0d233a235c12f2b
uohzxela <uohzxela@gmail.com>
2018-03-18 10:51:18 +0800

    Refactor bit_manipulation/reverse_digits.py

--- bit_manipulation/reverse_digits.py
+++ bit_manipulation/reverse_digits.py
@@ -1,11 +1,9 @@
 def reverse_digits(x):
-	is_negative = x < 0
-	x = abs(x)
-	res = 0
-	while x:
-		res = res * 10 + x % 10
-		x /= 10
-	return -res if is_negative else res
+	x_tmp, res = abs(x), 0
+	while x_tmp:
+		res = res * 10 + x_tmp % 10
+		x_tmp /= 10
+	return -res if x < 0 else res
 
 assert reverse_digits(-123) == -321
 assert reverse_digits(4522) == 2254
