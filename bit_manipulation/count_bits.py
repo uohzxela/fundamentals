cefe14865a34f88ee27b2a723c5b8cf3ffd001c9
uohzxela <uohzxela@gmail.com>
2018-03-22 08:50:21 +0800

    Add alternative solution for bit_manipulation/count_bits.py

--- bit_manipulation/count_bits.py
+++ bit_manipulation/count_bits.py
@@ -24,3 +24,30 @@ assert count_bits2(0b11) == 2
 assert count_bits2(0b01) == 1
 assert count_bits2(0b1010) == 2
 assert count_bits2(0b1111) == 4
+
+"""
+Try to grok this patterna and you will understand this solution.
+
+0: 0000
+1: 0001
+2: 0010
+3: 0011
+...
+8: 1000
+9: 1001
+...
+12: 1100
+13: 1101
+14: 1110
+15: 1111
+"""
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        res = [0]
+        for x in xrange(1, num+1):
+            res.append(res[x >> 1] + (x & 1))
+        return res
